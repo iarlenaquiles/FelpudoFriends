2c6889650230154043aafae6a37d2483a3d7ad61
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-10-21 20:35:00 -0300

    Criando o evento do click na lista

--- app/src/main/java/com/tecnologia/aquiles/felpudofriends/MainActivity.java
+++ app/src/main/java/com/tecnologia/aquiles/felpudofriends/MainActivity.java
@@ -40,6 +40,13 @@ public class MainActivity extends AppCompatActivity {
             i++;
         }
         minhaLista.setAdapter(meuAdaptador);
+
+        minhaLista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                
+            }
+        });
     }
 }
 
