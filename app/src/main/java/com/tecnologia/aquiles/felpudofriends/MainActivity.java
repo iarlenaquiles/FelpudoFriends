5c267b1c62f8f74f88372ab9486749eac958fe07
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-10-21 03:24:32 -0300

    Criando o adaptador do view

--- app/src/main/java/com/tecnologia/aquiles/felpudofriends/MainActivity.java
+++ app/src/main/java/com/tecnologia/aquiles/felpudofriends/MainActivity.java
@@ -1,8 +1,13 @@
 package com.tecnologia.aquiles.felpudofriends;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
@@ -61,4 +66,50 @@ class ViewPersonagem{
     ImageView icone;
     TextView titulo;
     TextView descricao;
+}
+
+class MeuAdaptador extends ArrayAdapter{
+    public MeuAdaptador(Context context, int resource) {
+        super(context, resource);
+    }
+
+    @Override
+    public void add(@Nullable Object object) {
+        super.add(object);
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View minhaView;
+        minhaView = convertView;
+        ViewPersonagem viewPersonagem;
+
+        if(convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            minhaView = inflater.inflate(R.layout.minha_celula, parent, false);
+
+            viewPersonagem = new ViewPersonagem();
+            viewPersonagem.icone = (ImageView) minhaView.findViewById(R.id.meuIcone);
+            viewPersonagem.titulo = (TextView) minhaView.findViewById(R.id.meuTitulo);
+            viewPersonagem.descricao = (TextView) minhaView.findViewById(R.id.descricao);
+
+            minhaView.setTag(viewPersonagem);
+        } else {
+            viewPersonagem = (ViewPersonagem) minhaView.getTag();
+        }
+
+        DadosPersonagem dadosPersonagem;
+        dadosPersonagem = (DadosPersonagem) this.getItem(position);
+        viewPersonagem.icone.setImageResource(dadosPersonagem.getIcone());
+        viewPersonagem.titulo.setText(dadosPersonagem.getTitulo());
+        viewPersonagem.descricao.setText(dadosPersonagem.getDescricao());
+
+        return minhaView;
+    }
 }
