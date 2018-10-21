cb39ef1210b6b6aa293c4d92f8aa440b4c9886c4
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-10-21 02:52:29 -0300

    Initial commit

---
+++ app/src/main/java/com/tecnologia/aquiles/felpudofriends/MainActivity.java
@@ -0,0 +1,22 @@
+package com.tecnologia.aquiles.felpudofriends;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String [] listaNomes = {"Felpudo", "Fofura", "Lesmo"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> meuAdaptador = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, android.R.id.text1, listaNomes);
+
+        ListView minhaLista = findViewById(R.id.minhaLista);
+        minhaLista.setAdapter(meuAdaptador);
+    }
+}
